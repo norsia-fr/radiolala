1f569a7b3c942447688ab7bb32e227a7d715e383
Norias <vous@exemple.com>
2026-06-15 20:56:44 +0200

    CI: workflow de release (dist.zip auto sur tag) + désactivation des source maps en production

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: '',
+  productionSourceMap: false,
   chainWebpack: config => {
     config.module
       .rule('markdown')
